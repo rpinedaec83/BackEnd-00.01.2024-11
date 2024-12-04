1e9112fd8205b387dbf2c906e8a01c7689298805
Roberto Pineda Lopez <rpineda@x-codec.net>
2024-12-03 21:00:37 -0500

    Clase 03122024 segunda parte

--- Semana03Sesion01/d19423/script.js
+++ Semana03Sesion01/d19423/script.js
@@ -1,4 +1,30 @@
+//cuando se hace click en el boton btnScript se lanza una ventana
 document.getElementById("btnScript").addEventListener("click",(e)=>{
-    e.preventDefault();
+    e.preventDefault()
     alert("Hola desde el script")
-})
+});
+/**
+ * La 
+ * Documentacion
+ * en 
+ * varias
+ * lineas
+ */
+
+//Variables
+var edad = 41;
+
+var edadDelAlumno = 25;
+
+let hobbies = "Aeromodelismo";
+
+var edad = "Cuarenta y uno";
+
+{
+    let hobbies = ""
+}
+
+//CONSTANTES
+
+const IGV = 18;
+IGV = 33;
