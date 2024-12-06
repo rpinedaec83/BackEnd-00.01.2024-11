a1b123d67bc2a1488ad6cb82126530a1696178c5
Roberto Pineda Lopez <rpineda@x-codec.net>
2024-12-05 20:59:32 -0500

    Clase 05122024 segunda parte

--- Semana03Sesion02/d19423/js/index.js
+++ Semana03Sesion02/d19423/js/index.js
@@ -13,17 +13,90 @@
 //     document.getElementById("resultado").innerText = `Bienvenido: ${txtNombre}`;
 // })
 
-console.log("Inicio Sumadora");
+// console.log("Inicio Sumadora");
 
-let primerNumero = Number.parseFloat( prompt("Escribe el primer numero"));
-let segundoNumero = Number.parseFloat( prompt("Escribe el Segundo numero"));
+// let primerNumero = Number.parseFloat( prompt("Escribe el primer numero"));
+// let segundoNumero = Number.parseFloat( prompt("Escribe el Segundo numero"));
 
-console.log(typeof(primerNumero));
+// console.log(typeof(primerNumero));
 
-if(isNaN(primerNumero) || isNaN(segundoNumero)){
-    alert("Escribe correctamente los datos")
-}else{
-    let resultado = primerNumero + segundoNumero;
-    alert(`El resultado de la suma es: ${resultado}`);
+// if(isNaN(primerNumero) || isNaN(segundoNumero)){
+//     alert("Escribe correctamente los datos")
+// }else{
+//     let resultado = primerNumero + segundoNumero;
+//     alert(`El resultado de la suma es: ${resultado}`);
+// }
+
+// console.log("Inicio de la calculadora V1");
+
+
+// let primerNumero = Number.parseFloat(prompt("Escribe el primer numero"));
+// let segundoNumero = Number.parseFloat(prompt("Escribe el Segundo numero"));
+// let operacion = Number.parseInt(prompt(`Escribe la operacion:
+//     1   ->   Suma
+//     2   ->   Resta
+//     3   ->   Multiplicacion
+//     4   ->   Division` ));
+
+// if (isNaN(primerNumero) || isNaN(segundoNumero) || isNaN(operacion)) {
+//     alert("Escribe correctamente los datos")
+// } else {
+//     let resultado = 0;
+//     switch (operacion) {
+//         case 1:
+//             resultado = primerNumero + segundoNumero;
+//             break;
+//         case 2:
+//             resultado = primerNumero - segundoNumero;
+//             break;
+//         case 3:
+//             resultado = primerNumero * segundoNumero;
+//             break;
+//         case 4:
+//             resultado = primerNumero / segundoNumero;
+//             break;
+//         default:
+//             alert(`La operacion ${operacion} no esta implementada`)
+//             break;
+//     }
+//     alert(`El resultado de la suma es: ${resultado}`);
+// }
+
+
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+    
 }
 
+let arrNotas = [20,16,14,16,12,15];
+let sumatoria = 0;
+let promedio = 0
+for (let index = 0; index < arrNotas.length; index++) {
+    sumatoria += arrNotas[index];
+
+}
+console.log(promedio = sumatoria/arrNotas.length);
+
+
+ sumatoria = 0;
+ promedio = 0
+arrNotas.forEach(element => {
+    sumatoria +=element;
+});
+console.log(promedio = sumatoria/arrNotas.length);
+
+
+let bandera = true;
+let inicio = 0;
+let fin = 10
+do {
+    inicio++;
+    if(inicio==fin){
+        bandera = false;
+    }
+    console.log(inicio)
+} while (bandera);
+
+// while (condition) {
+    
+// }
