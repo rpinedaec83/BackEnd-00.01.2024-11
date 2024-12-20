9538288a2b10e38232ffc2311644de9c04faa61f
Roberto Pineda Lopez <rpineda@x-codec.net>
2024-12-19 20:32:51 -0500

    Agregando clases

--- Semana05Sesion02/d19423/js/scripts.js
+++ Semana05Sesion02/d19423/js/scripts.js
@@ -1,1 +1,29 @@
-// Empty JS for your own code to be here
+const Reserva = function () //a
+{
+    let empresa = "";
+    let ciudad ="";
+
+    function configurar(){
+        // document.getElementById("empresa").innerText = empresa ;
+        $("#empresa").text(empresa);
+        $("#ciudad").text(ciudad);
+    }
+
+    function eventos(){
+        $("#btnReservar").on("click", function(e){
+            e.preventDefault();
+            console.log("Hizo click en reservar");
+            
+        })
+    }
+
+    return {
+        init: function(parametros){
+            empresa = parametros.nombre;
+            ciudad = parametros.ciudad;
+            console.log(parametros);
+            configurar();
+            eventos();
+        }
+    }
+}();
