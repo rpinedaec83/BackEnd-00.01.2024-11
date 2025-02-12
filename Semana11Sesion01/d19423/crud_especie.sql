7e6f15e7e86bf258f51a27f3c6e7576d7e41970e
Roberto Pineda Lopez <rpineda@x-codec.net>
2025-02-11 21:31:26 -0500

    Procedures

---
+++ Semana11Sesion01/d19423/crud_especie.sql
@@ -0,0 +1,41 @@
+drop procedure if exists crud_especie;
+
+
+DELIMITER $$
+CREATE PROCEDURE crud_especie (in opcion int, in pdescripcion varchar(100), in pactivo bit,in pid int, in pusuario int)
+BEGIN
+	IF opcion = 1 then
+		begin
+			select * from especie where is_active = 1;
+		end;
+    elseif opcion = 2 then
+		begin
+			insert into especie(descripcion, usuario_creacion)
+			values(pdescripcion, pusuario);
+		end;
+	elseif opcion = 3 then
+		begin
+			update especie
+            set descripcion = ifnull(pdescripcion, descripcion),
+            is_active = ifnull(pactivo, is_active),
+            usuario_modificacion = pusuario,
+            fecha_modificacion = current_timestamp()
+            where id=pid;
+			
+		end;
+	elseif opcion = 4 then
+		begin
+			update especie
+            set is_active = 0,
+            usuario_modificacion = pusuario,
+            fecha_modificacion = current_timestamp()
+            where id=pid;
+			
+		end;
+	elseif opcion = 5 then
+		begin
+			select * from especie where id = pid;
+		end;
+    END IF;
+END $$
+DELIMITER ;
