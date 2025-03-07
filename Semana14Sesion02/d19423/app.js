4db08b71fcea72eba41247efd2d83ddfdd677573
Roberto Pineda Lopez <rpineda@x-codec.net>
2025-03-06 21:52:15 -0500

    Clase 06032025 segunda parte

--- Semana14Sesion02/d19423/app.js
+++ Semana14Sesion02/d19423/app.js
@@ -7,27 +7,46 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+const OpenAI = require('openai');
+const openai = new OpenAI({
+    apiKey: process.env.OPEN_AI_APIKEY
+})
+
 const PORT = process.env.PORT || 8080;
 app.use(express.static("public"));
 
 
 io.on("connection",(socket)=>{
     console.log("Nuevo usuario conectado");
-    socket.on("sendMessage", (message, callback)=>{
+    const historialConversacion = [];
+    historialConversacion.push({
+        role:"system",
+        content: `Eres un chatbot de una empresa que vende helados, tienes 10 sabores de helados que 
+        incluyen: Fresa, mango, chocolate entre otros, el sabor mas recomendado es el de Vainilla, tu mision es vender estos helados`
+    })
+    socket.on("sendMessage", async (message, callback)=>{
         try {
             console.log(message)
-            switch (message) {
-                case "Hola":
-                    socket.emit("message", "Que tal");
-                    break;
-            
-                default:
-                    break;
-            }
-            
+            historialConversacion.push({role:"user", content: message});
+            const respuestaChat = await openai.chat.completions.create({
+                model: process.env.OPEN_AI_MODEL || "gpt-3.5-turbo",
+                messages: historialConversacion
+            });
+            const respuesta = respuestaChat.choices[0].message?.content;
+            historialConversacion.push({role:"assistant", content: respuesta});
+            socket.emit("message", respuesta);
             callback();
-        } catch (error) {
+            // switch (message) {
+            //     case "Hola":
+            //         socket.emit("message", "Que tal");
+            //         break;
             
+            //     default:
+            //         break;
+            // }
+            console.log(historialConversacion)
+        } catch (error) {
+            console.log(error)
         }
     });
     socket.on("disconnect", ()=>{console.log("Usuario Desconectado")})
